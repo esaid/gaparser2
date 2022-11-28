d68aa0e860277443bd3cf1111ef872b6db4f3f2f
Ga144 <emmanuel.said@cern.ch>
2022-11-28 22:56:06 +0100

    Update gaparser2.py

--- gaparser2.py
+++ gaparser2.py
@@ -1,7 +1,9 @@
 import pyutil
 from pyutil import inany
+import re
 
 directoryExamples = '/examples'
+directoryBibliotheqque = 'Libraries/'
 comserial = "com9"  # le port serie
 compilega144 = True  # permet de voir sous forme json le resultat de la compilation
 programga144 = False  # programmation du ga144
@@ -18,14 +20,31 @@ def read_file(File_input):
 def list_find_(string_found):
     list_require = []
     for i in list_code:
-        if inany(i, 'require', True):
-            list_require.append(i)
+        if inany(i, string_found, True):
+            list_require.append(re.sub(string_found, '', i).strip() + '.ga')
     return list_require
 
 
 code = read_file(file_ga)
-print(code)
+print(f"code:  {code}")
 list_code = list(map(str.strip, code.splitlines()))
-print(list_code)
+print(f"list_code:  {list_code}")
+list_bibliotheque = list_find_('require')
+print(f"list_bibliotheque {list_bibliotheque}")
+list_code_bibliotheque = []
+for ll in list_bibliotheque:
+    code_bibliotheque = read_file(directoryBibliotheqque + ll)
+    print(f"code_bibliotheque {ll}  {code_bibliotheque}")
+    list_code_bibliotheque.append(re.split(r":", code_bibliotheque))
 
-print(list_find_('require'))
+print(f"list_code_bibliotheque : {list_code_bibliotheque}")
+
+
+code_to_add=[]
+for lc in list_code:
+    print(lc)
+    for lcb in list_code_bibliotheque:
+        print(lcb)
+        if lc in lcb:
+            code_to_add.append(": " + lcb)
+print(f"code a ajouter : {code_to_add}")
