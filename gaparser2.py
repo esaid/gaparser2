6ddcb83f842d1e19c5ef4b712549c6c73270445d
esaid <emmanuel.said@cern.ch>
2022-11-29 02:15:44 +0100

    lecture fichier liste du code sans espaces list_find('require') list_code list_code_bibliotheque code_to_add code_remplace

--- gaparser2.py
+++ gaparser2.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyutil
 from pyutil import inany, do
 import re
@@ -33,12 +34,18 @@ print(f"list_code:  {list_code}")
 list_bibliotheque = list_find_('require')
 print(f"list_bibliotheque {list_bibliotheque}")
 list_code_bibliotheque = []
+code_bibliotheque = ""
 for ll in list_bibliotheque:
-    code_bibliotheque = read_file(directoryBibliotheqque + ll)
+    code_bibliotheque += read_file(directoryBibliotheqque + ll)
     # print(f"code_bibliotheque {ll}  {code_bibliotheque}")
     list_code_bibliotheque.append(re.split(r":", code_bibliotheque))
 list_code_bibliotheque = sum(list_code_bibliotheque, [])  # remove list [[][]]
 print(f"list_code_bibliotheque : {list_code_bibliotheque}")
+list_mot_code_bibliotheque = list(filter(lambda x: x != '', list(map(str.strip, code_bibliotheque.splitlines()))))
+list_mot_code_bibliotheque = list(map(lambda each: each.strip(": "), list_mot_code_bibliotheque))
+list_mot_code_bibliotheque = list(map(lambda each: each.strip("( n )"), list_mot_code_bibliotheque))
+
+print(f"list_mot_code_bibliotheque : {list_mot_code_bibliotheque}")
 
 code_to_add = []
 code_to_replace = []
@@ -49,5 +56,25 @@ for lc in list_code:
             print(lc, lcb)
             code_to_add.append(": " + lcb)
             break
+    for lmcb in list_mot_code_bibliotheque:
+        if inany(lc, lmcb, True):
+            print(lmcb, lc)
+            code_to_replace.append(lmcb)
 code_to_add = set(code_to_add)
+code_to_replace = set(code_to_replace)
 print(f"code a ajouter : {code_to_add}")
+print(f"code a remplacer: {code_to_replace}")
+
+code_remplace = []
+def find_index(code):
+    for m in code_to_replace:
+        m = " " + m + ' '
+        for ind in list_code_bibliotheque:
+            if inany(ind, m, True):
+                code.append(ind)
+                break
+    return code
+
+
+find_index(code_remplace)
+print(f"code a remplacer {code_remplace}")
