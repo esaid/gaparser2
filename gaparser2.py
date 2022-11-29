19146eb14e86781870da035ce88a6b19c2ab7f6a
esaid <emmanuel.said@cern.ch>
2022-11-29 10:53:06 +0100

    lecture fichier liste du code sans espaces list_find('require') list_code list_code_bibliotheque code_to_add code_remplace dict change dictionnaire code_bibliotheque

--- gaparser2.py
+++ gaparser2.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyutil
 from pyutil import inany, do, fileappend, fileoverwrite, filereplace, toDict
 import re
-
+import sys
 directoryExamples = '/examples'
 directoryBibliotheqque = 'Libraries/'
 comserial = "com9"  # le port serie
@@ -19,29 +19,55 @@ def read_file(File_input):
         return f.read()
 
 
-def list_find_(string_found):
+def list_find_(string_found, s_extension):
     list_require = []
     for i in list_code:
         if inany(i, string_found, True):
-            list_require.append(re.sub(string_found, '', i).strip() + '.ga')
+            list_require.append(re.sub(string_found, '', i).strip() + s_extension)
     return list_require
 
+def find_between(s, start, end):
+    return ': ' + (s.split(start))[1].split(end)[0] + " ;"
 
+
+# code source
 code = read_file(file_ga)
 print(f"code:  {code}")
+# liste_code , separation des lignes , suppression espaces
 list_code = list(filter(lambda x: x != '', list(map(str.strip, code.splitlines()))))
-
 print(f"list_code:  {list_code}")
-list_bibliotheque = list_find_('require')
+
+# list_bibliotheque fichiers
+list_bibliotheque = list_find_('require', '.ga')
 print(f"list_bibliotheque {list_bibliotheque}")
+
 list_code_bibliotheque = []
 code_bibliotheque = ""
 for ll in list_bibliotheque:
     code_bibliotheque += read_file(directoryBibliotheqque + ll)
     # print(f"code_bibliotheque {ll}  {code_bibliotheque}")
-    list_code_bibliotheque.append(re.split(r":", code_bibliotheque))
-list_code_bibliotheque = sum(list_code_bibliotheque, [])  # remove list [[][]]
-print(f"list_code_bibliotheque : {list_code_bibliotheque}")
+    #list_code_bibliotheque.append(re.split(r":", code_bibliotheque))
+#list_code_bibliotheque = list(filter(lambda x: x != '', list(map(str.strip, code_bibliotheque.splitlines()))))
+
+
+
+#list_code_bibliotheque = sum(list_code_bibliotheque, [])  # remove list [[][]]
+# print(f"code_bibliotheque : {code_bibliotheque}")
+dict_code_bibliotheque = {}
+while not code_bibliotheque:
+    cle_code_ = find_between(code_bibliotheque, ': ', ' ;')
+
+    cle_code_bibliotheque , code_ = cle_code_.split("\n")[0].replace(': ',''), cle_code_
+    print(f" cle: {cle_code_bibliotheque} , code  {code_}")
+    dict_code_bibliotheque[cle_code_bibliotheque] = code_
+    print(f" dictionnaire code bibliotheque {dict_code_bibliotheque}")
+
+    # clean code_bibliotheque
+    code_bibliotheque = code_bibliotheque.replace(code_, '')
+    print(f"code biblioheque {code_bibliotheque}")
+
+
+sys.exit()
 list_mot_code_bibliotheque = list(filter(lambda x: x != '', list(map(str.strip, code_bibliotheque.splitlines()))))
 list_mot_code_bibliotheque = list(map(lambda each: each.strip(": "), list_mot_code_bibliotheque))
 list_mot_code_bibliotheque = list(map(lambda each: each.strip("( n )"), list_mot_code_bibliotheque))
@@ -67,6 +93,8 @@ print(f"code a ajouter : {code_to_add}")
 print(f"code a remplacer: {code_to_replace}")
 
 code_remplace = []
+
+
 def find_index(code):
     for m in code_to_replace:
         m = " " + m
@@ -80,20 +108,16 @@ def find_index(code):
 find_index(code_remplace)
 print(f"code a remplacer {code_remplace}")
 
-
-
 print(type(code_to_replace))
 print(type(code_remplace))
 
-
 dict_a_remplacer = toDict(list(code_to_replace), code_remplace)
 for cle, valeur in dict_a_remplacer.items():
-        print("l'élément de clé", cle, "vaut", valeur)
-
+    print("l'élément de clé", cle, "vaut", valeur)
 
 fileoverwrite(file_ga_, code)
 for s_code_to_add in code_to_add:
-    fileappend(file_ga_ , s_code_to_add )
+    fileappend(file_ga_, s_code_to_add)
 
-#for s_code_to_replace in code_remplace:
-    # filereplace(file_ga_ , s_code_to_add, s_code_to_replace )
+# for s_code_to_replace in code_remplace:
+# filereplace(file_ga_ , s_code_to_add, s_code_to_replace )
