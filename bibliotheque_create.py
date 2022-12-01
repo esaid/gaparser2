c0b0d0ad382ba4d4393d54e39a7cf36cd1d06d9a
esaid <emmanuel.said@cern.ch>
2022-12-01 21:21:03 +0100

    lecture fichier liste du code sans espaces list_find('require') list_code list_code_bibliotheque code_to_add code_remplace dict change dictionnaire code_bibliotheque plus simple simplification code_to_add code_to_replace filleappend filereplace  if inany(lc, key):  clean require  ga-tools  modification find_file_bibliotheque map, lambda  dans le code  mot \  signifie remplacer  mot  signifie ajouter a la fin du code  fonction bibliotheque dans fichier bibliotheque_create.py

---
+++ bibliotheque_create.py
@@ -0,0 +1,50 @@
+# lecture fichier
+from pyutil import inany
+
+
+def read_file(File_input):
+    with open(File_input, 'r') as f:
+        return f.read()
+
+
+# list bibliotheque
+def find_files_bibliotheque(list_, string_found, s_extension):
+    # cherche index de string_found dans list_, et retourne la liste des fichiers ( sans string_found )  + s_extension
+    # exemple require delay ( index 0 ) require gpio ( index 1 ) , retourne [delay.ga, gpio.ga]
+    return list(
+        map(lambda s_: (list_[s_].replace(string_found, '').strip() + s_extension),
+            [i_ for i_, value_ in enumerate(list_) if inany(value_, string_found, True)]))
+
+
+# decoupage definition et code
+def find_between(s, start, end):
+    return ': ' + (s.split(start))[1].split(end)[0] + " ;"
+
+
+def creation_dictionnaire(code_bibliotheque_, s_fin):
+    dict_code_bibliotheque = {}
+    while code_bibliotheque_:
+        cle_code_ = find_between(code_bibliotheque_, ': ', ' ;')
+
+        cle_code_bibliotheque, code_ = cle_code_.split("\n")[0].replace(': ', ''), cle_code_
+        # print(f" cle: {cle_code_bibliotheque} , code  {code_}")
+        if inany(cle_code_bibliotheque, s_fin, True):
+            break
+        cle_code_bibliotheque = cle_code_bibliotheque.split(' ')[0]
+        dict_code_bibliotheque[cle_code_bibliotheque] = code_.replace(': ', '')
+        # print(f" dictionnaire code bibliotheque {dict_code_bibliotheque}")
+
+        # clean code_bibliotheque
+        code_bibliotheque_ = code_bibliotheque_.replace(code_, '')
+        # print(f"code biblioheque {code_bibliotheque_}")
+
+    return dict_code_bibliotheque
+
+
+def dictionnaire_bibliotheque_total(list_bibliotheque_, directoryBibliotheque_):
+    dict_bibliotheque_ = {}
+    for ll in list_bibliotheque_:
+        code_bibliotheque__ = read_file(directoryBibliotheque_ + ll)
+        dict_bibliotheque_.update(creation_dictionnaire(code_bibliotheque__, ll.replace('.ga', '')))
+        # print(f"code_bibliotheque {code_bibliotheque__}")
+    return dict_bibliotheque_
--- gaparser2.py
+++ gaparser2.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pyutil
-from pyutil import inany, do, fileappend, fileoverwrite, filereplace, toDict
+from  bibliotheque_create import read_file, find_files_bibliotheque,  dictionnaire_bibliotheque_total
+from pyutil import inany, fileappend, fileoverwrite, filereplace
 import re
 import sys
 import os
 
 directoryExamples = '/examples'
-directoryBibliotheqque = 'Libraries/'
+directoryBibliotheque = 'Libraries/'
 comserial = "com9"  # le port serie
 compilega144 = True  # permet de voir sous forme json le resultat de la compilation
 programga144 = False  # programmation du ga144
@@ -15,77 +16,21 @@ file_ga = "examples/ledpulse.ga"
 file_ga_ = file_ga + '_'
 
 
-# lecture fichier
-def read_file(File_input):
-    with open(File_input, 'r') as f:
-        return f.read()
-
-
-# list bibliotheque
-'''
-def list_find_(string_found, s_extension):
-    list_require = []
-    for i in list_code:
-        if inany(i, string_found, True):
-            list_require.append(re.sub(string_found, '', i).strip() + s_extension)
-    return list_require
-'''
-
-
-def find_files_bibliotheque(list_, string_found, s_extension):
-    return list(
-        map(lambda s_: (list_[s_].replace(string_found,'').strip() + s_extension), [i for i, value in enumerate(list_) if inany(value,string_found, True)]))
-
-
-# decoupage definition et code
-def find_between(s, start, end):
-    return ': ' + (s.split(start))[1].split(end)[0] + " ;"
-
-
-def creation_dictionnaire(code_bibliotheque_, s_fin):
-    dict_code_bibliotheque = {}
-    while code_bibliotheque_:
-        cle_code_ = find_between(code_bibliotheque_, ': ', ' ;')
-
-        cle_code_bibliotheque, code_ = cle_code_.split("\n")[0].replace(': ', ''), cle_code_
-        # print(f" cle: {cle_code_bibliotheque} , code  {code_}")
-        if inany(cle_code_bibliotheque, s_fin, True):
-            break
-        cle_code_bibliotheque = cle_code_bibliotheque.split(' ')[0]
-        dict_code_bibliotheque[cle_code_bibliotheque] = code_.replace(': ', '')
-        # print(f" dictionnaire code bibliotheque {dict_code_bibliotheque}")
-
-        # clean code_bibliotheque
-        code_bibliotheque_ = code_bibliotheque_.replace(code_, '')
-        # print(f"code biblioheque {code_bibliotheque_}")
-
-    return dict_code_bibliotheque
-
-
-def dictionnaire_bibliotheque_total(list_bibliotheque_):
-    dict_bibliotheque_ = {}
-    for ll in list_bibliotheque_:
-        code_bibliotheque__ = read_file(directoryBibliotheqque + ll)
-        dict_bibliotheque_.update(creation_dictionnaire(code_bibliotheque__, ll.replace('.ga', '')))
-        # print(f"code_bibliotheque {code_bibliotheque__}")
-    return dict_bibliotheque_
-
-
 # code source
 code = read_file(file_ga)
-print(f"code:  {code}")
+print(f"code: \n{code}")
 # liste_code , separation des lignes , suppression espaces
-list_code = list(filter(lambda x: x != '', list(map(str.strip, code.splitlines()))))
-#list_code = list(filter(lambda x: x != '', list(map(str.strip, code.split()))))
-print(f"list_code:  {list_code}")
+list_code = list(filter(lambda x: x != '', list(map(str.strip, code.splitlines()))))  # separation lignes
+# list_code = list(filter(lambda x: x != '', list(map(str.strip, code.split()))))
+#print(f"list_code:  {list_code}")
 
 # list_bibliotheque fichiers
 # list_bibliotheque = list_find_('require', '.ga')
 list_bibliotheque = find_files_bibliotheque(list_code, 'require', '.ga')
-print(f"list_bibliotheque {list_bibliotheque}")
+print(f"list_bibliotheque : {list_bibliotheque}")
 
-dict_bibliotheque = dictionnaire_bibliotheque_total(list_bibliotheque)
-print(f"dictionnaire_bibliotheque {dict_bibliotheque}")
+dict_bibliotheque = dictionnaire_bibliotheque_total(list_bibliotheque, directoryBibliotheque)
+#print(f"dictionnaire_bibliotheque {dict_bibliotheque}")
 
 code_to_add = []
 code_to_replace = []
@@ -104,8 +49,8 @@ code_to_add = code_to_add[1::2]  # odd element
 # clean code_to_replace
 
 
-print(f"code a ajouter : {code_to_add}")
-print(f"code a remplacer: {code_to_replace}")
+#print(f"code a ajouter : {code_to_add}")
+#print(f"code a remplacer: {code_to_replace}")
 
 # init code
 fileoverwrite(file_ga_, code)
@@ -123,8 +68,8 @@ for i in range(len(code_to_replace) - 1, -1, -2):
     filereplace(file_ga_, ' ' + s_code_to_found, s_code_to_replace)
 
 # code source
-code = read_file(file_ga_)
-print(code)
+newcode = read_file(file_ga_)
+print(f" nouveau code: \n{newcode}")
 
 if compilega144:
     # "python ga.py examples/boutoninput_.ga --json"
